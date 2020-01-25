8ee01e62b7aa44879a429f425ab2423e02f46dfa
Calob Humble <cshumble@protonmail.com>
2020-01-25 16:39:42 +0000

    rewrite, more accurate

--- colorSensor.py
+++ colorSensor.py
@@ -4,74 +4,32 @@ import busio
 import adafruit_tcs34725
 import numpy as np
 
-
-
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
-sensor.integration_time = 50
+sensor.integration_time = 100
 
-global latch_counter
-latch_counter = 0
-latch = 0
+def DetectControlPanelColor():
+    colors = sensor.color_rgb_bytes
+    r = np.floor(colors[0]/20)
+    g = np.floor(colors[1]/20)
+    b = np.floor(colors[2]/20)
+    if ( r > 0 and g == 0 and b == 0):
+        return [1, 0, 0, 0]
+    if (r == 0 and g > 0 and b == 0):
+        return [0, 1, 0, 0]
+    if (r == 0 and g >= 0 and b > 0):
+        return [0, 0, 1, 0]
+    if (r > 0 and g > 0 and b == 0):
+        return [0, 0, 0, 1]
+    return [0, 0, 0, 0]
 
-global color_detected
-color_detected = 0 # red=1, yel=2, grn=3, blu=4
-global last_color_detected
-last_color_detected = 0
-
-red = np.array([170, 100, 85], dtype=np.uint16)
-grn = np.array([95, 170, 120], dtype=np.uint16)
-blu = np.array([100, 200, 220], dtype=np.uint16)
-yel = np.array([270, 280, 145], dtype=np.uint16)
-
-def Count():
-    global latch_counter
-    global last_color_detected
-    # asks if it saw this color before, if so, then it increments the counter, if not, then it resets it
-    if last_color_detected == color_detected:
-        latch_counter += 1
-    else:
-        latch_counter = 0
-    #assigns current color to last color for future loops
-    last_color_detected = color_detected
-
-def Detect():
-    global color_detected
-    detected_list = ('r', 'g', 'b', 'c')
-    x = list(detected_list)
-   
-    x[0] = sensor.color_raw[0]
-    x[1] = sensor.color_raw[1]
-    x[2] = sensor.color_raw[2]
-    x[3] = sensor.color_raw[3]
-    #print('r:{0}, g:{1}, b:{2}, c:{3}'.format(x[0], x[1], x[2], x[3]))
+def DetectionToColorName(det):
+    if (det[0]): return "Red"
+    elif (det[1]): return "Green"
+    elif (det[2]): return "Cyan"
+    elif (det[3]): return "Yellow"
+    else: return "No Color Detected"
     
-    
-    if x[0] > (yel[0] -  20) and x[0] < (yel[0] + 20) and x[1] > (yel[1] - 20) and x[1] < (yel[1] + 20) and x[2] > (yel[2] - 20) and x[2] < (yel[2] + 20):
-        color_detected = 2
-
-    elif x[0] > (red[0] - 20) and x[0] < (red[0] + 20) and x[1] > (red[1] - 20) and x[1] < (red[1] + 20) and x[2] > (red[2] - 20) and x[2] < (red[2] + 20):
-        color_detected = 1
-        
-    elif x[0] > (grn[0] - 20) and x[0] < (grn[0] + 20) and x[1] > (grn[1] - 20) and x[1] < (grn[1] + 20) and x[2] > (grn[2] - 20) and x[2] < (grn[2] + 20):
-        color_detected = 3
-        
-    elif (x[0] > (blu[0] - 20)) and (x[0] < (blu[0] + 20)) and (x[1] > (blu[1] - 20)) and (x[1] < (blu[1] + 20)) and (x[2] > (blu[2] - 20)) and (x[2] < (blu[2] + 20)):
-        color_detected = 4
-
-
-    
-
 while True:
-    Detect()
-    Count()
-    
-    if latch_counter == 3:
-        if color_detected == 1:
-            print('I see RED')
-        elif color_detected == 3:
-            print('I see GREEN')
-        elif color_detected == 4:
-            print('I see BLUE')
-        elif color_detected == 2:
-            print('I see YELLOW')
+    colordet = DetectControlPanelColor()
+    print(DetectionToColorName(colordet))
